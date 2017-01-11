7c77a16176415b6e7f5c8ee83f9009d22a111fc9
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-01-11 17:58:08 +0100

    demo

---
+++ src/main/java/pl/sda/App.java
@@ -0,0 +1,19 @@
+package pl.sda;
+
+import org.apache.commons.mail.EmailException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws EmailException, IOException {
+//        MailSender.sendMailWithMessageFromFile("javandwro1@gmail.com",
+//                "Hello mail from JWrabel #3", "file.txt");
+
+
+    }
+}
---
+++ src/test/java/pl/sda/AppTest.java
@@ -0,0 +1,13 @@
+package pl.sda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
